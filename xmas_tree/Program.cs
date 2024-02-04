cc8c9481fc893f6df6fd39dffb7fc147b1cee1c9
HWShawn <ondlraehsconegrb@gmail.com>
2024-02-05 06:11:49 +0800

    加入專案檔案。

---
+++ xmas_tree/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xmas_tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 9;
+            for (int i = 0; i <= width / 2; i++)
+            {
+                int ubound = 2 * i + 1;
+                int pading = width / 2 - i;
+                string star = "";
+                for (int j = 1; j <= ubound; j++)
+                {
+                    star += "*";
+                }
+                star = star.PadLeft(pading + ubound, ' ');
+                Console.WriteLine(star);
+            }
+            Console.WriteLine();
+        }
+    }
+}
